31590339f3e2459dab6115d25c6718e3d44f6781
Truong Vy <truongvy99413@gmail.com>
2020-12-14 20:00:19 +0700

    update room

--- app.js
+++ app.js
@@ -16,21 +16,30 @@ app.use(cors());
 
 // Socket IO
 const server = http.createServer(app);
+
+// cors cho host
+// const io = require('socket.io')(server,{
+//     cors:{
+//         origin:process.REACT_APP_client_domain,
+//         method: ["GET","POST"],
+//         allowHeaders: ["*"],
+//         credentials: true
+// }
+// });
+
+//cors cho local
 const io = require('socket.io')(server,{
-    cors:{
-        origin:process.env.REACT_APP_client_domain,
-        method: ["GET","POST"],
-        allowHeaders: ["*"],
-        credentials: true
-}
+    cors:true,
+    origin:process.REACT_APP_client_domain
 });
 
-let userOnline = []; //danh sách user dang online
+let userOnline = []; //danh sách user dang online: userOnline[x][y] => x là thứ tự người onl, y = 0 là socket id, y = 1 là id user, y = 2 là tên user
+let playRooms = []; //danh sách bàn 
+
 
 io.on('connection', function(socket) {
     //lắng nghe khi người dùng thoát
     socket.on('disconnect', function() {
-        console.log(socket.id + ': disconnected')
         var disconnectedUserID;
         for (var a=0; a < userOnline.length; a++) {
             if (userOnline[a][0] === socket.id) {
@@ -38,8 +47,7 @@ io.on('connection', function(socket) {
                 userOnline.splice(disconnectedUserID, 1);
             }
         }
-        
-        io.sockets.emit('updateUesrList', userOnline);
+        io.sockets.emit('updateUsersOnlineList', userOnline);
     })
     //lắng nghe khi có người login
     socket.on('login', data=> {
@@ -49,9 +57,10 @@ io.on('connection', function(socket) {
             if(userOnline.length==0){
                 userOnline.push(userlogin);
                 // console.log(userOnline);
-                io.sockets.emit('updateUesrList', userOnline);// gửi danh sách user dang online
+                io.sockets.emit('updateUsersOnlineList', userOnline);// gửi danh sách user dang online
             }
             else{
+                //kiểm tra trùng user
                 var flag=0;
                 for (var a=0; a < userOnline.length; a++) {
                     if (userOnline[a][1] == userlogin[1]) {
@@ -62,13 +71,76 @@ io.on('connection', function(socket) {
                 { 
                     userOnline.push(userlogin);
                     // console.log(userOnline);
-                    io.sockets.emit('updateUesrList', userOnline);// gửi danh sách user dang online
+                    io.sockets.emit('updateUsersOnlineList', userOnline);// gửi danh sách user dang online
                 }
             }
-
         }
-    })
-    console.log(userOnline);
+        io.sockets.emit('updateRoomsList', playRooms);// gửi danh sách room dang tồn tại
+    });
+
+    socket.on('createRoom', dataRoom => {
+        console.log('create new room');
+        newRoomID = playRooms.length +1;
+        // new room :
+        // #0 id; 
+        // #1 tên người tạo;
+        // #2 trạng thái: 0 = chờ , 1 = đang chơi;
+        // #3 id player 1; 
+        // #4 name player 1; 
+        // #5 id player 2;
+        // #6 name player 2
+        newRoom = [newRoomID, dataRoom,0,null,null,null,null];
+        playRooms.push(newRoom);
+        io.sockets.emit('updateRoomsList', playRooms);
+        console.log(playRooms);
+    });
+
+    socket.on('joinRoom', data => { //data là room id
+        console.log('new user join room ', data);
+        for (var a=0; a < playRooms.length; a++) {
+            if (playRooms[a][0] == data) {
+                socket.join(data); //join room theo room id
+                io.sockets.to(data).emit('roomJoined',playRooms[a]);//gui thong tin room vừa join                
+            }
+        }
+        io.sockets.emit('updateRoomsList', playRooms);
+        console.log(playRooms);
+    });
+
+    socket.on('updateRoom', data => { //data là room id
+        for (var a=0; a < playRooms.length; a++) {
+            if (playRooms[a][0] == data[0]) {
+                playRooms[a][3] = data[1];
+                playRooms[a][4] = data[2];
+                playRooms[a][5] = data[3];
+                playRooms[a][6] = data[4];
+                console.log("update:" + playRooms[a]);
+                io.sockets.emit('roomUpdated',playRooms[a]);//gui thong tin room vừa join
+            }
+        }
+        io.sockets.emit('updateRoomsList', playRooms);
+        console.log(playRooms);
+    });
+
+    socket.on('leaveRoom', data => { //data là id player
+        for (var a=0; a < playRooms.length; a++) {
+            if (playRooms[a][3] === data) {
+                playRooms[a][3] = null;
+                playRooms[a][4] = null;
+                console.log("update:" + playRooms[a]);
+                io.sockets.emit('roomUpdated',playRooms[a]);//gui thong tin room vừa join
+            }
+            if (playRooms[a][5] === data) {
+                playRooms[a][5] = null;
+                playRooms[a][6] = null;
+                console.log("update:" + playRooms[a]);
+                io.sockets.emit('roomUpdated',playRooms[a]);//gui thong tin room vừa join
+            }
+        }
+        io.sockets.emit('updateRoomsList', playRooms);
+        console.log(playRooms);
+    });
+
 });
 
 // Connect mongoDB
